1f2fc4e5c1217a84116652f6af22020813b92c2b
sndyareeba <sam20brian@gmail.com>
2021-01-13 13:13:22 +0300

    Create Database.php

---
+++ Database.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "localhost";
+$dBUsername = "root";
+$dBPassword = "";
+
+$dBName = "iap";
+
+$conn = mysqli_connect($servername,$dBUsername,$dBPassword, $dBName);
+
+
+
+if (!$conn){
+	die("Connection Failed".mysqli_connect_error());
+}
+?>
+
